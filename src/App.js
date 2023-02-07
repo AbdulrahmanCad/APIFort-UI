88b57072c770a6a43f34ac6eb3b1793c1929c62c
Abdulrahman Cad <abdulrahmancadura@gmail.com>
2023-02-07 23:17:28 +0300

    Adding fonts to app.css + adding routes to the app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, {lazy} from "react"
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider} from '@mui/material/styles';
 
+const Sidebar = lazy (() => import("./components/Sidebar/Sidebar"))
 const Profile = lazy (() => import("./pages/Profile"))
+const Endpoint = lazy (() => import("./pages/Endpoint"))
+
+var mainBgStyle = {
+  backgroundColor : '#F4F3EE',
+  height: '100vh',
+  display: "flex",
+  minHeight : '100vh'
+}
+const theme = createTheme({
+  typography: { fontFamily: ["Yellowtail", "Poppins"].join(",") },
+  transitions: {
+    easing: {
+      easeInOut: 'cubic-bezier(.25,.1,.36,1.18)',
+    },
+  },
+});
 
 function App() {
   return (<>
+  <ThemeProvider theme={theme}>
+  <Sidebar />
+  <div style={mainBgStyle}>
+  
   <Router>
     <Routes>
+      <Route path="/profile" element={<Profile />}></Route>
+      <Route path="/profile/:id" element={<Endpoint />}></Route>
       <Route path="*" element={<Profile />}></Route>
     </Routes>
   </Router>
-  
+  </ div>
+  </ThemeProvider>
   </>);
 }
 
